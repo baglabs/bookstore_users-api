097eb18901274e51875742e6a93518807b2216e1
Bayu Aditya <bayu_aditya@linkaja.id>
2022-07-21 12:05:04 +0700

    dao structure

---
+++ controllers/users/users_controller.go
@@ -0,0 +1,50 @@
+package users
+
+import (
+	"bookstore_user-api/domain/users"
+	"bookstore_user-api/services"
+	"bookstore_user-api/utils/errors"
+	"net/http"
+	"strconv"
+
+	"github.com/gin-gonic/gin"
+)
+
+func CreateUser(c *gin.Context) {
+	var user users.User
+
+	if err := c.ShouldBindJSON(&user); err != nil {
+		restErr := errors.NewBadRequestError("Invalid json body")
+		c.JSON(restErr.Status, restErr)
+		return
+	}
+
+	result, saveErr := services.CreateUser(user)
+	if saveErr != nil {
+		c.JSON(saveErr.Status, saveErr)
+		return
+	}
+
+	c.JSON(http.StatusCreated, result)
+}
+
+func GetUser(c *gin.Context) {
+	userId, userErr := strconv.ParseInt(c.Param("user_id"), 10, 64)
+	if userErr != nil {
+		err := errors.NewBadRequestError("invalid user id should be a number")
+		c.JSON(err.Status, err)
+		return
+	}
+
+	user, getErr := services.GetUser(userId)
+	if getErr != nil {
+		c.JSON(getErr.Status, getErr)
+		return
+	}
+
+	c.JSON(http.StatusOK, user)
+}
+
+// func SearchUser(c *gin.Context) {
+// 	c.String(http.StatusNotImplemented, "implement me!")
+// }
